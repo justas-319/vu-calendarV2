1a42d425bae136185b26992d2a67bf72ea0afafa
sronhatie <237258465+sronhatie@users.noreply.github.com>
2026-09-17 13:53:23 +0300

    created an initial react app (#6)
    * added node_modules to be ignored
    
    * connected React to ASP.NET
    
    * scripts to run the servers + formatters
    
    * added newline at the end of file

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello, world!");
+app.MapGet("/api/message", () => "message from c#");
 
 app.Run();
